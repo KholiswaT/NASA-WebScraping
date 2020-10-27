40fa8e08af6af4c1c9cbe5a2774723a6b5913423
KholiswaT <67981626+KholiswaT@users.noreply.github.com>
2020-10-27 00:21:44 -0400

    Update scrape_mars.py

--- scrape_mars.py
+++ scrape_mars.py
@@ -1,7 +1,7 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import re
-
+import pandas as pd
 
 def init_browser():
     executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
