097cb7afe8064dcf77b8b57d3f5b2a1e65354995
KholiswaT <67981626+KholiswaT@users.noreply.github.com>
2020-10-28 10:20:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/"
 mongo = PyMongo(app)
 
 
